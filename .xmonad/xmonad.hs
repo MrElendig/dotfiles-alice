646bcf95851d01932a1cfbfe266f9cb0462adb46
oh <mrelendig@har-ikkje.net>
2009-04-09 19:40:34 +0200

    Added keybindings for my new TwinView setup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -153,5 +153,11 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [((m .|. modMask, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    -- mod-[w,e] %! switch to twinview screen 1/2
+    -- mod-shift-[w,e] %! move window to screen 1/2
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 -------------------------------------------------------------------------------
